ce75c004ea6bd39a8f07a27affd464e28cf807e3
sunil3696 <sunilstark3696@gmail.com>
2024-09-28 08:26:47 -0400

    wrote a code to connect to mongodb.

--- assignment-1_Express_API/server.js
+++ assignment-1_Express_API/server.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const MongodbURI = "mongodb+srv://sunil:sunil123@cluster0.df1iq.mongodb.net/";
 
+mongoose.connect(MongodbURI,{
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Success fully Connected to MongoDB'))
+.catch((err) => console.error(err));
 
 app.use(express.json());
 
