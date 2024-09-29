85ca55f911aec9c71cea841f01e549ae3184c689
sunil3696 <sunilstark3696@gmail.com>
2024-09-29 01:29:36 -0400

    tested apis

--- assignment-1_Express_API/src/controller/recipeController.js
+++ assignment-1_Express_API/src/controller/recipeController.js
@@ -4,7 +4,7 @@ const Recipe = require('../models/recipeModel');
 const getAllRecipes = async (req, res) => {
     try {
         const recipes = await Recipe.find();
-        // res.json("I am here");
+        // res.json("I am here sending respinses");
         res.status(200).json(recipes);
     }
     catch (error) {
