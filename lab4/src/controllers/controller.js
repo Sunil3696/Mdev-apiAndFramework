f1858987254abd584a760ca23c9a2eadb899e304
sunil3696 <sunilstark3696@gmail.com>
2024-10-11 00:24:34 -0400

    adding genre filter and checking the working condition

--- lab4/src/controllers/controller.js
+++ lab4/src/controllers/controller.js
@@ -56,26 +56,13 @@ exports.getMovies = async(req,res)=>{
         if(req.query.title) {
             //adding query title to the filter and making the filter incase sensitive
             filter.title = new RegExp(req.query.title, 'i');
-            let movies = await Movie.find(filter);
-                if(movies.length > 0){
-                    // returning movies data
-                    res.status(200).send(movies);
-                } else {
-                    res.status(404).send("No movie found with that title");
-                }
+        }
+        if (req.query.genre) {
+            filter.genres = req.query.genre;
         }
 
-        else {
-            //returning all movies (same code as previos version)
-            try{
-                        const movies = await Movie.find();
-                         res.status(200).json(movies);
-                    }
-                    catch(e){
-                            console.error(e);
-                            res.status(500).send('Error retrieving Movies');
-                    }
-        }
+        console.log(filter)
+        return
     }
     catch (error) {
         console.error('Error fetching movies:', error);
