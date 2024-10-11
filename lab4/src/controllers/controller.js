055130eb2c67df5372f1b14ffc4cd1547745c11f
sunil3696 <sunilstark3696@gmail.com>
2024-10-10 23:22:58 -0400

    started to extend the filter functionality

--- lab4/src/controllers/controller.js
+++ lab4/src/controllers/controller.js
@@ -13,42 +13,76 @@ const fs =require('fs');
 //     }
 // };
 
-exports.getMovies = async(req,res)=>{
-        // console.log(req.query.title);
-        // return;
-        try {
-            let filter = {};
-            if(req.query.title) {
-                //adding query title to the filter and making the filter incase sensitive
-                filter.title = new RegExp(req.query.title, 'i');
-                let movies = await Movie.find(filter);
-                    if(movies.length > 0){
-                        // returning movies data
-                        res.status(200).send(movies);
-                    } else {
-                        res.status(404).send("No movie found with that title");
-                    }
-            }
+//version 1 of movie search by title
+// exports.getMovies = async(req,res)=>{
+//         // console.log(req.query.title);
+//         // return;
+//         try {
+//             let filter = {};
+//             if(req.query.title) {
+//                 //adding query title to the filter and making the filter incase sensitive
+//                 filter.title = new RegExp(req.query.title, 'i');
+//                 let movies = await Movie.find(filter);
+//                     if(movies.length > 0){
+//                         // returning movies data
+//                         res.status(200).send(movies);
+//                     } else {
+//                         res.status(404).send("No movie found with that title");
+//                     }
+//             }
 
-            else {
-                //returning all movies (same code as previos version)
-                try{
-                            const movies = await Movie.find();
-                             res.status(200).json(movies);
-                        }
-                        catch(e){
-                                console.error(e);
-                                res.status(500).send('Error retrieving Movies');
-                        }
-            }
+//             else {
+//                 //returning all movies (same code as previos version)
+//                 try{
+//                             const movies = await Movie.find();
+//                              res.status(200).json(movies);
+//                         }
+//                         catch(e){
+//                                 console.error(e);
+//                                 res.status(500).send('Error retrieving Movies');
+//                         }
+//             }
+//         }
+//         catch (error) {
+//             console.error('Error fetching movies:', error);
+//             res.status(500).send({ message: 'Error retrieving movies' });
+//         }
+
+// }
+
+exports.getMovies = async(req,res)=>{
+    try {
+        let filter = {};
+        if(req.query.title) {
+            //adding query title to the filter and making the filter incase sensitive
+            filter.title = new RegExp(req.query.title, 'i');
+            let movies = await Movie.find(filter);
+                if(movies.length > 0){
+                    // returning movies data
+                    res.status(200).send(movies);
+                } else {
+                    res.status(404).send("No movie found with that title");
+                }
         }
-        catch (error) {
-            console.error('Error fetching movies:', error);
-            res.status(500).send({ message: 'Error retrieving movies' });
+
+        else {
+            //returning all movies (same code as previos version)
+            try{
+                        const movies = await Movie.find();
+                         res.status(200).json(movies);
+                    }
+                    catch(e){
+                            console.error(e);
+                            res.status(500).send('Error retrieving Movies');
+                    }
         }
+    }
+    catch (error) {
+        console.error('Error fetching movies:', error);
+        res.status(500).send({ message: 'Error retrieving movies' });
+    }
 
 }
-
 
 
 
