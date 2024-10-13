3141076d21c7658cc6140adb72141dc785f6a2b9
sunil3696 <sunilstark3696@gmail.com>
2024-10-13 04:47:44 -0400

    worked on loginuser function code on controller

--- Assignment2/src/controller/userController.js
+++ Assignment2/src/controller/userController.js
@@ -20,17 +20,34 @@ const registerUser = async (req, res) => {
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        console.error(error);
+        res.status(500).send('Error occured');
     }
 }
 
 
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
-
+    try {
     if(!email || !password) {
         res.status(401).json({ message: 'All fields are required' });
         return
+    }
+
+    const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isPasswordValid = await user.isValidPassword(password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful', userId: user._id });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error occured');
     }
 };
 
