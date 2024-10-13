6d8567354ab4b2be9ee85328b87498519c807280
sunil3696 <sunilstark3696@gmail.com>
2024-10-13 04:36:44 -0400

    modified user model

--- Assignment2/src/controller/userController.js
+++ Assignment2/src/controller/userController.js
@@ -1,11 +1,29 @@
 const User = require("../models/userModel");
 
 const registerUser = async (req, res) => {
-    // res.send("I am register")
+    const { email, password } = req.body;
+    const user = new User({ email, password });
+    try {
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
 
 
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+};
 
 
 
-module.exports = {registerUser}
+
+
+module.exports = {registerUser, loginUser}
--- Assignment2/src/models/userModel.js
+++ Assignment2/src/models/userModel.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
 
-const User = mongoose.model("User", userSchema);
+
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.isValidPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
