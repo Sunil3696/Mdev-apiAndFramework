c6e366e11d00b382cb463f68300f31ce28da1f43
sunil3696 <sunilstark3696@gmail.com>
2024-10-13 05:28:42 -0400

    worked on logout controller function

--- Assignment2/src/controller/userController.js
+++ Assignment2/src/controller/userController.js
@@ -53,7 +53,12 @@ const loginUser = async (req, res) => {
 
 
 const logoutUser = (req, res) => {
-   
+    req.session.destroy(err => {
+        if (err) {
+            return res.status(500).json({ message: 'Error logging out user' });
+        }
+        res.status(200).json({ message: 'Logout successful' });
+    });
 };
 
 
