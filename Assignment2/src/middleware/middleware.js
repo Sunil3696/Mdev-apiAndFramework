89b695c4f8f923dd8eb79ccb6d5e5d49e669e68c
sunil3696 <sunilstark3696@gmail.com>
2024-10-13 06:04:48 -0400

    added more comment

--- Assignment2/src/middleware/middleware.js
+++ Assignment2/src/middleware/middleware.js
@@ -1,3 +1,10 @@
+
+/**
+ * Desc: Middleware that checks if all the fields are not empty
+ *Parameter:
+ *   req : recipe contents
+ *   res :check the condition and if any of the fileds are empty then return error ot go for another middleware
+ */
 const validateRecipe = (req, res, next) => {
     const { recipeName, ingredients, cookingTime,difficulty, cuisine, description,photoLink,averageRating } = req.body;
 
