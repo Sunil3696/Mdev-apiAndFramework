e61743ad28e6dc0ed852da650951425a2560f281
sunil3696 <sunilstark3696@gmail.com>
2024-10-13 04:42:53 -0400

    worked on user registration and password encryption

--- Assignment2/src/controller/userController.js
+++ Assignment2/src/controller/userController.js
@@ -1,8 +1,21 @@
 const User = require("../models/userModel");
 
 const registerUser = async (req, res) => {
-    const { email, password } = req.body;
-    const user = new User({ email, password });
+    const { email, password , username} = req.body;
+
+    if(!email || !password || !username){
+        res.status(400).json({ message: 'All fields are required' });
+        return 
+    }
+
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+         res.status(400).json({ message: 'Email already in use' });
+         return
+    }
+
+    const user = new User({ email, password , username});
     try {
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
